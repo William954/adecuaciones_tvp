d6c587a3a956920af43a11367ecc09cb4a7a1e88
William <williamcolin954@gmail.com>
2019-06-14 04:20:10 -0500

    templates

--- tvp_test/__manifest__.py
+++ tvp_test/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'static/src/xml/chatter.xml',
+        # 'static/src/xml/chatter.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
